caa0c2d3e9f033995b2a7becea6467a690872884
Andrea Pferscher <andrea.pferscher@student.tugraz.at>
2016-04-05 15:23:37 +0200

    [HO, PF] {test before code} mainActivityTest added

---
+++ LittleWhale/app/src/androidTest/java/com/example/andrea/littewhale/uitest/MainActivityTest.java
@@ -0,0 +1,41 @@
+package com.example.andrea.littewhale.uitest;
+
+import android.test.ActivityInstrumentationTestCase2;
+import com.example.andrea.littewhale.MainActivity;
+import com.robotium.solo.Solo;
+
+
+/**
+ * Created by andrea on 05.04.16.
+ */
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    private Solo mySolo;
+
+    public MainActivityTest() {
+        super(MainActivity.class);
+    }
+
+    public void setUp() throws Exception {
+        super.setUp();
+        mySolo = new Solo(getInstrumentation(), getActivity());
+
+    }
+
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void testStartNavigationBtn() {
+        mySolo.clickOnButton("Start Navigating");
+    }
+
+    public void testEditLocationsBtn() {
+        mySolo.clickOnButton("Edit Locations");
+    }
+
+
+
+
+
+}
