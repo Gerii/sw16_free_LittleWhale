9325aae035f271ae5dafaec092bf109801ca2add
Clemens Hofer <clemens.hofer@student.tugraz.at>
2016-04-08 00:18:52 +0200

    [HO,PF] {test before code} simple list implemented

--- LittleWhale/app/src/main/java/com/example/andrea/littewhale/EditLocations.java
+++ LittleWhale/app/src/main/java/com/example/andrea/littewhale/EditLocations.java
@@ -5,9 +5,20 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import com.example.andrea.littewhale.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class EditLocations extends AppCompatActivity {
+
+    List valueList = new ArrayList<String[]>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +35,27 @@ public class EditLocations extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        for(int i = 0; i < 10; i++){
+            valueList.add("value " + i);
+        }
+
+        ListAdapter adapter = new ArrayAdapter<String>(getApplicationContext(), R.layout.custom_list_item, valueList);
+
+        final ListView lv = (ListView)findViewById(R.id.listView);
+
+        lv.setAdapter(adapter);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3)
+            {
+                String blub = lv.getAdapter().getItem(arg2).toString();
+                Log.w("w", blub);
+            }
+        });
+
     }
 
 }
