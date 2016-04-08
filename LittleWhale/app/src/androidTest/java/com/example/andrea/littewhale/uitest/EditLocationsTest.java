b23b409ff28bb4257d46d017c58143bc140ab5e1
Clemens Hofer <clemens.hofer@student.tugraz.at>
2016-04-08 14:15:44 +0200

    [HO,PF] {code after test} 2line-list with test

--- LittleWhale/app/src/androidTest/java/com/example/andrea/littewhale/uitest/EditLocationsTest.java
+++ LittleWhale/app/src/androidTest/java/com/example/andrea/littewhale/uitest/EditLocationsTest.java
@@ -1,7 +1,11 @@
 package com.example.andrea.littewhale.uitest;
 
+import android.app.Instrumentation;
 import android.test.ActivityInstrumentationTestCase2;
+import android.util.Log;
 import android.view.View;
+import android.widget.AbsListView;
+import android.widget.ListView;
 
 import com.example.andrea.littewhale.EditLocations;
 import com.example.andrea.littewhale.R;
@@ -9,12 +13,37 @@ import com.robotium.solo.Solo;
 
 import junit.framework.TestCase;
 
+import java.util.ArrayList;
+
 /**
  * Created by clemens on 05.04.16.
  */
 public class EditLocationsTest extends ActivityInstrumentationTestCase2 {
 
     private Solo mySolo;
+
+    public View getViewAtIndex(final ListView listElement, final int indexInList, Instrumentation instrumentation) {
+        ListView parent = listElement;
+        if (parent != null) {
+            if (indexInList <= parent.getAdapter().getCount()) {
+                scrollListTo(parent, indexInList, instrumentation);
+                int indexToUse = indexInList - parent.getFirstVisiblePosition();
+                return parent.getChildAt(indexToUse);
+            }
+        }
+        return null;
+    }
+
+    public <T extends AbsListView> void scrollListTo(final T listView,
+                                                     final int index, Instrumentation instrumentation) {
+        instrumentation.runOnMainSync(new Runnable() {
+            @Override
+            public void run() {
+                listView.setSelection(index);
+            }
+        });
+        instrumentation.waitForIdleSync();
+    }
 
     public EditLocationsTest(){
         super(EditLocations.class);
@@ -34,5 +63,13 @@ public class EditLocationsTest extends ActivityInstrumentationTestCase2 {
         mySolo.clickOnView(fab);
     }
 
+    public  void testList(){
+        ListView lv = (ListView) getActivity().findViewById(R.id.listView);
 
+        for(int i = 0; i < lv.getAdapter().getCount() ; i++){
+            mySolo.clickOnView(getViewAtIndex(lv, i, getInstrumentation()));
+            Log.w("w", i + "a");
+            mySolo.sleep(500);
+        }
+    }
 }
--- LittleWhale/app/src/main/java/com/example/andrea/littewhale/EditLocations.java
+++ LittleWhale/app/src/main/java/com/example/andrea/littewhale/EditLocations.java
@@ -11,14 +11,18 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
 import com.example.andrea.littewhale.R;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class EditLocations extends AppCompatActivity {
 
-    List valueList = new ArrayList<String[]>();
+    List<Map<String, String>> valueList = new ArrayList<Map<String, String>>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,35 +31,38 @@ public class EditLocations extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fabAdd);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
+        //listview
         for(int i = 0; i < 10; i++){
-            valueList.add("value " + i);
+            Map<String, String> tmp = new HashMap<String, String>(2);
+            tmp.put("line1", "line" + (i + 1));
+            tmp.put("line2", "value of second line" + (i + 1));
+            valueList.add(tmp);
         }
 
-        ListAdapter adapter = new ArrayAdapter<String>(getApplicationContext(), R.layout.custom_list_item, valueList);
+        final ListAdapter adapter = new SimpleAdapter(this, valueList,
+                android.R.layout.simple_list_item_2,
+                new String[] {"line1", "line2" },
+                new int[] {android.R.id.text1, android.R.id.text2 });
 
         final ListView lv = (ListView)findViewById(R.id.listView);
 
         lv.setAdapter(adapter);
 
-        lv.setOnItemClickListener(new AdapterView.OnItemClickListener()
-        {
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3)
-            {
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
                 String blub = lv.getAdapter().getItem(arg2).toString();
-                Log.w("w", blub);
+                Log.w("w", blub + " " + arg3);
             }
         });
 
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fabAdd);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.w("fab", "fab pressed");
+            }
+        });
     }
 
 }
