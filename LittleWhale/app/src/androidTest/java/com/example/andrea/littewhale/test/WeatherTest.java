a651d5ee884fd7e1727da14b50b005fa74dc3215
Gerii <stofei@sbg.at>
2016-05-30 17:42:00 +0200

    [PA,PF] Weather test

---
+++ LittleWhale/app/src/androidTest/java/com/example/andrea/littewhale/test/WeatherTest.java
@@ -0,0 +1,23 @@
+package com.example.andrea.littewhale.test;
+import android.test.AndroidTestCase;
+
+import com.example.andrea.utils.Weather;
+import com.example.andrea.utils.WeatherParsingException;
+
+
+/**
+ * Created by gery on 30.05.16.
+ */
+public class WeatherTest extends AndroidTestCase{
+
+    public void testUpdateWeather() {
+        Weather testWeather = new Weather();
+        try {
+            testWeather.updateWeather(45.12, 44.13);
+        } catch (WeatherParsingException e) {
+            assertTrue(false);
+        }
+
+
+    }
+}
